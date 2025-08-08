68c0423971d723f4763ad206df535c914dec44cc
Alicehhhmm <a007aplum@gmail.com>
2025-08-08 19:41:26 +0800

    feat:  init EditorDrawingBoard

--- src/pages/editor/components/editor-drawing-board.jsx
+++ src/pages/editor/components/editor-drawing-board.jsx
@@ -1,11 +1,61 @@
-import React from 'react'
+'use client'
 
-import { useEditor } from '@/pages/editor/components'
+import { useRef } from 'react'
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch'
+import { cn } from '@/lib/utils'
+
+import { useEditor, Toolbar } from '@/pages/editor/components'
+import { useTransformLang } from '@/hooks'
+import { MeshBackground } from '@/components/common'
 
 function EditorDrawingBoard({ children }) {
-    const {} = useEditor()
+    // hooks
 
-    return <main className='flex-1 overflow-auto p-6 bg-accent/60'>{children}</main>
+    const { t } = useTransformLang()
+    const { layoutMode, meshMode } = useEditor()
+
+    // States
+
+    const wrapperRef = useRef(null)
+
+    // Method
+
+    const isSinglePage = layoutMode === 'single'
+    const isMultiPage = layoutMode === 'multi'
+
+    return (
+        <TransformWrapper
+            ref={wrapperRef}
+            centerOnInit
+            minScale={0.4}
+            maxScale={2}
+            initialScale={0.8}
+            limitToBounds={false}
+            wheel={{ step: 0.05, wheelDisabled: true }}
+            panning={{ wheelPanning: true }}
+        >
+            <div className='!w-full !h-screen !relative'>
+                {meshMode && <MeshBackground model='lines' size='sm' colors='gray' />}
+
+                {/* TODO: Role components */}
+
+                <Toolbar />
+                <TransformComponent
+                    wrapperClass='!size-full !relative'
+                    contentClass={cn(
+                        'bg-white pointer-events-none',
+                        isSinglePage && 'flex flex-col items-start justify-center space-y-12 ',
+                        isMultiPage && 'grid items-start justify-center space-x-12 '
+                    )}
+                >
+                    {/* TODO: Paging component */}
+                    {/* <div className='bg-amber-300 w-30 h-10'>page1</div>
+                    <div className='bg-green-300 w-30 h-10'>page2</div> */}
+                    {children}
+                </TransformComponent>
+            </div>
+        </TransformWrapper>
+    )
 }
 
 export default EditorDrawingBoard
