6501ad8a266a9f9c40b45aec1d7f31a3f5da4a5e
Alicehhhmm <a007aplum@gmail.com>
2025-08-17 20:30:38 +0800

    chore(pages): added settings pages

--- src/pages/resume/[resumeId]/settings/index.jsx
+++ src/pages/resume/[resumeId]/settings/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { LanguagesIcon } from 'lucide-react'
+
+import { ThemeToggle } from '@/components/common'
+
+import { cn } from '@/lib/utils'
+import { useTransformLang } from '@/hooks'
+
+const SectionRow = ({ title, description, className, children }) => {
+    return (
+        <div className={cn('flex flex-col gap-1.5', className)}>
+            <div className='space-y-1'>
+                <label className='text-base font-medium'>{title}</label>
+                {description && <p className='text-sm text-muted-foreground'>{description}</p>}
+            </div>
+            <div className='flex-shrink-0'>{children}</div>
+        </div>
+    )
+}
 
 function SettingsPage() {
-    return <div>SettingsPage </div>
+    const { t } = useTransformLang()
+    return (
+        <div className='flex flex-col gap-4 p-4'>
+            <header className='space-y-1'>
+                <h1 className='text-xl font-semibold'>{t('systemSettings')}</h1>
+                <p className='text-sm text-muted-foreground'>Adjust the default settings of the system</p>
+            </header>
+            <div className='border-b'></div>
+            <SectionRow title={t('themeToggle')}>
+                <div className='flex'>
+                    <ThemeToggle />
+                </div>
+            </SectionRow>
+
+            <SectionRow title={t('languages')}>
+                {/* DOTO: use i18n <LanguagesToggle /> */}
+                <LanguagesIcon />
+            </SectionRow>
+        </div>
+    )
 }
 
 export default SettingsPage
