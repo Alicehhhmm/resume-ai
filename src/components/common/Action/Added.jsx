d5d173b99b2856c223c11dbb3b06b31b93808155
Alicehhhmm <a007aplum@gmail.com>
2025-08-30 16:35:34 +0800

    fix(components): update added

--- src/components/common/Action/Added.jsx
+++ src/components/common/Action/Added.jsx
@@ -4,16 +4,22 @@ import { SquarePlusIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 
-export const Added = forwardRef(({ cursor = 'pointer', ...props }, ref) => {
+export const Added = forwardRef(({ cursor = 'pointer', children, ...props }, ref) => {
     return (
         <Button
             ref={ref}
             variant='ghost'
             size='icon'
-            className={cn('size-8 active:scale-90 text-muted-foreground/60 active:text-primary', cursor && `cursor-${cursor}`)}
+            className={cn(
+                'size-8 active:scale-90 text-muted-foreground/60 active:text-primary',
+                cursor && `cursor-${cursor}`,
+                'hover:bg-muted hover:text-primary',
+                'transition-colors duration-200'
+            )}
             {...props}
         >
-            <SquarePlusIcon size='16' />
+            {!children && <SquarePlusIcon size='16' />}
+            {children}
         </Button>
     )
 })
