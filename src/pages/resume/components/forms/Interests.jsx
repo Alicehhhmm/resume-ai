e97bfebaeb6abd6347d1eaf273e32dadd2996ade
Alicehhhmm <a007aplum@gmail.com>
2025-09-14 15:25:40 +0800

    chore(pages): update ResumeEditPanel sections

---
+++ src/pages/resume/components/forms/Interests.jsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import { LoaderCircle } from 'lucide-react'
+import { useParams } from 'react-router-dom'
+import { v4 as uuidv4 } from 'uuid'
+
+import { toast } from 'sonner'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+import { Accordion } from '@/components/ui/accordion'
+
+import { RichTextEditor, CollapsiblePanel } from '@/components/common'
+
+import { useResumeEdit, useTransformLang } from '@/hooks'
+
+import { UpdateResumeDetail } from '@/api/apis/resume'
+
+const uuid = uuidv4()
+
+const initialFormField = {
+    id: uuid,
+    name: '',
+    link: '',
+    startDate: '',
+    endDate: '',
+    position: '',
+    description: '',
+    summary: '',
+}
+
+function Interests() {
+    return (
+        <>
+            <h2>Interests</h2>
+        </>
+    )
+}
+
+export default Interests
--- src/pages/resume/components/forms/index.js
+++ src/pages/resume/components/forms/index.js
@@ -6,8 +6,21 @@ export { default as HeaderSetion } from './HeaderSetion'
 export const PersonalDetail = lazy(() => import('./PersonalDetail'))
 export const Education = lazy(() => import('./Education'))
 export const Experience = lazy(() => import('./Experience'))
-export const Skills = lazy(() => import('./Skills'))
 export const Summary = lazy(() => import('./Summary'))
+export const Projects = lazy(() => import('./Projects'))
+export const Certifications = lazy(() => import('./Certifications'))
+export const Award = lazy(() => import('./Award'))
+
+export const Skills = lazy(() => import('./Skills'))
+export const Languages = lazy(() => import('./Languages'))
+export const Publications = lazy(() => import('./Publications'))
+export const Interests = lazy(() => import('./Interests'))
+export const Volunteer = lazy(() => import('./Volunteer'))
+export const Internship = lazy(() => import('./Internship'))
+export const Portfolio = lazy(() => import('./Portfolio'))
+export const References = lazy(() => import('./References'))
+
+export const Custom = lazy(() => import('./Custom'))
 
 // Module Components
 
@@ -17,20 +30,20 @@ export const MODULE_COMPONENTS = {
     'section-summary': Summary,
     'section-education': Education,
     'section-experience': Experience,
-    'section-projects': '',
-    'section-certifications': '',
-    'section-awards': '',
+    'section-projects': Projects,
+    'section-certifications': Certifications,
+    'section-awards': Award,
 
     /* === OPTIONAL === */
     'section-skills': Skills,
-    'section-languages': '',
-    'section-publications': '',
-    'section-interests': '',
-    'section-volunteer': '',
-    'section-internship': '',
-    'section-portfolio': '',
-    'section-references': '',
+    'section-languages': Languages,
+    'section-publications': Publications,
+    'section-interests': Interests,
+    'section-volunteer': Volunteer,
+    'section-internship': Internship,
+    'section-portfolio': Portfolio,
+    'section-references': References,
 
     /* === CUSTOM === */
-    'section-custom': '',
+    'section-custom': Custom,
 }
