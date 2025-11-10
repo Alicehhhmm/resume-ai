08b6a4a899de105626b5f92f7bc1acf0af9e0ddf
Alicehhhmm <a007aplum@gmail.com>
2025-11-10 18:53:29 +0800

    chore(template): use typography to tmp

--- src/pages/template/standard/SummaryPreview.jsx
+++ src/pages/template/standard/SummaryPreview.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function SummaryPreview({ resumeInfo }) {
-    return <p className='my-4 text-sm leading-relaxed text-muted-foreground'>{resumeInfo?.profile}</p>
+    return (
+        <section className='my-4 text-sm leading-relaxed text-muted-foreground'>
+            <div className='wysiwyg' dangerouslySetInnerHTML={{ __html: resumeInfo.profile }} />
+        </section>
+    )
 }
 
 export default SummaryPreview
